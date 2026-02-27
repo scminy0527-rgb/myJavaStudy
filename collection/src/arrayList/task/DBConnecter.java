113e076d2aacfa6afb94d3452010eb197f46a535
scminy0527-rgb <scminy0527@gmail.com>
2026-02-27 13:44:35 +0900

    feat: deleteUser

--- collection/src/arrayList/task/DBConnecter.java
+++ collection/src/arrayList/task/DBConnecter.java
@@ -4,5 +4,4 @@ import java.util.ArrayList;
 
 public class DBConnecter {
 	public static ArrayList<User> users = new ArrayList<User>();
-	
 }
--- collection/src/arrayList/task/UserField.java
+++ collection/src/arrayList/task/UserField.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class UserField {
 	public ArrayList<User> users = DBConnecter.users;
 	public final int KEY = 300;
+	public static String session;
 //	1. 회원가입
 //	- id : x,
 //	- name, password, phone
@@ -13,7 +14,7 @@ public class UserField {
 	public User checkId(String id) {
 		for(User user: users) {
 			if(user.getId().equals(id)) {
-				System.out.println("이미 사용중입니다.");
+//				System.out.println("이미 사용중입니다.");
 				return user;
 			}
  		}
@@ -48,29 +49,90 @@ public class UserField {
 //	- 찾으면 password 검사
 //	- 찾은 user 를 리턴
 	public User login(User user) {
-		for(User dbUser: users) {
-//			db 에서 일단 id 가 존재하는 유저를 찾았을 때
-			if(user.equals(dbUser)) {
-//				일단 아이디가 존재
-//				- 비밀번호 검증
-				String encodedPw = encode(user.getPassword());
-				if(encodedPw.equals(dbUser.getPassword())) {
-					System.out.println("로그인 성공, 환영합니다.");
-//					db 에 있는 해당 유저를 진짜로 반환 해야 함
-					return dbUser;
-				} else {
-					System.out.println("비밀번호 오류 입니다.");
-					return null;
-				}
+		User dbUser = checkId(user.getId());
+		if(dbUser != null) {
+			String encodedPw = encode(user.getPassword());
+			if(encodedPw.equals(dbUser.getPassword())) {
+				System.out.println("로그인 성공, 환영합니다.");
+//				db 에 있는 해당 유저를 진짜로 반환 해야 함
+				session = dbUser.getId();
+				return dbUser;
+			} else {
+				System.out.println("비밀번호 오류 입니다.");
+				return null;
 			}
 		}
-//		여기까지 돌았음에도 없는거
 		System.out.println("아이디가 존재하지 않습니다.");
 		return null;
 	}
 	
+	
+//	public User login(User user) {
+//		for(User dbUser: users) {
+////			db 에서 일단 id 가 존재하는 유저를 찾았을 때
+//			if(user.equals(dbUser)) {
+////				일단 아이디가 존재
+////				- 비밀번호 검증
+//				String encodedPw = encode(user.getPassword());
+//				if(encodedPw.equals(dbUser.getPassword())) {
+//					System.out.println("로그인 성공, 환영합니다.");
+////					db 에 있는 해당 유저를 진짜로 반환 해야 함
+//					session = dbUser.getId();
+//					return dbUser;
+//				} else {
+//					System.out.println("비밀번호 오류 입니다.");
+//					return null;
+//				}
+//			}
+//		}
+////		여기까지 돌았음에도 없는거
+//		System.out.println("아이디가 존재하지 않습니다.");
+//		return null;
+//	}
+	
+	
+	public void logout() {
+//		현재 세션에 id 가 저장이 된 경우에만 로그아웃을 해야 함
+//		즉 로그인 된 상태 에서만 로그아웃이 가능
+		if(session != null) {
+//			로그아웃 진행
+			System.out.println("로그아웃 진행합니다.");
+			session = null;
+		} else {
+//			로그인 도 안했는데 로그아웃을 하겠다는 거
+			System.out.println("이미 로그아웃이 되었습니다.");
+		}
+	}
+	
+//	회원 탈퇴
+//	1. session 에 정보가 있는지 확인
+//	2. 있다면 db 에서 유저 정보 삭제
+//	3. session 에서 최종적으로 정보 지우기
+	public void deleteUser() {
+		if(session != null) {
+			User user = checkId(session);
+			if(users.contains(user)) {
+				users.remove(user);
+				session = null;
+				System.out.println("그동안 감사했습니다.");
+			} else {
+				System.out.println("유효하지 않은 회원 입니다.");
+			}
+		} else {
+			System.out.println("세션 정보 오류");
+		}
+	}
+	
+//	비밀번호 변경
+	
+//	인증번호 전송
+	
+//	인증번호 확인
+	
 	public static void main(String[] args) {
 //		3. 로그아웃
+		
+		
 //		4. 회원 탈퇴
 //		5. 비밀번호 변경(비밀번호 변경 30일)
 //		5. 비밀번호 변경(비밀번호 변경 30일)
@@ -79,6 +141,7 @@ public class UserField {
 		UserField uf = new UserField();
 		User user1 = new User("hong123", "홍길동", "1234", "010-1234-1234");
 		User user2 = new User("hong123", "김길홍", "1234", "010-4567-4567");
+		
 		
 		uf.join(user1);
 		uf.join(user2);
@@ -92,5 +155,13 @@ public class UserField {
 		System.out.println(uf.login(tempForLogin));
 		System.out.println(uf.login(tempForLogin2));
 		System.out.println(uf.login(tempForLogin3));
+		
+		uf.logout();
+		uf.logout();
+		System.out.println(uf.login(tempForLogin2));
+		System.out.println(uf.users);
+		uf.deleteUser();
+		System.out.println(uf.users);
+		
 	}
 }
